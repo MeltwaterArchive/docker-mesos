3771e08783519a7d84208d9db984257b12e4548f
Mikael Johansson <mikael.johansson@meltwater.com>
2015-08-12 14:02:21 +0200

    Added request headers to webapp response for debug purposes

--- demo-webapp/webapp/app.py
+++ demo-webapp/webapp/app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -10,11 +11,15 @@ def hello():
 """
 Hello world!<br/><br/>
 - I'm container id %s<br/>
-- My database is at %s
+- My database is at %s<br/>
+<br/>
+== HTTP Request Headers ==<br/>
+%s
 """
     cid = os.environ.get('HOSTNAME','localhost')
     dsn = os.path.expandvars(os.environ.get('DATABASE',''))
-    return template % (cid, dsn)
+    headers = "<br/>\n".join(['%s: %s' % (key, value) for key, value in request.headers.items()])
+    return template % (cid, dsn, headers)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
