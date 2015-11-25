8722ac6d5a3b1cd11fcf4b2d9969fbc1e5e4c24d
Mikael Johansson <mikael.johansson@meltwater.com>
2015-11-25 15:41:02 +0100

    Use Lighter to deploy to Marathon. Added config volume and inotifywait loop so ligther redeploys on file save

--- marathon-submit/bin/launch.sh
+++ marathon-submit/bin/launch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-for file in $@; do
+for file in /submit/json/*.json; do
   while true; do
     curl -X POST -H "Content-Type: application/json" "${MARATHON_URL}/v2/apps?force=true" -d "@${file}"
     if [ "$?" == "0" ]; then
@@ -10,6 +10,15 @@ for file in $@; do
   done
 done
 
-while true; do
-  sleep 10000 & wait
-done
+# Run lighter to deploy yaml files
+function DEPLOY {
+    /bin/lighter -v deploy -f -m "${MARATHON_URL}" $(find /submit/site -name \*.yml -not -name globals.yml)
+}
+
+DEPLOY;
+EVENTS="CREATE,CLOSE_WRITE,DELETE,MODIFY,MOVED_FROM,MOVED_TO"
+
+inotifywait -m -q -e "$EVENTS" -r --format '%:e %w%f' "/submit/site" | while read file
+  do
+    DEPLOY;
+  done
