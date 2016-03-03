984f6984b35f9a6aefb514d526d24fcbb8307f01
Mikael Johansson <mikael.johansson@meltwater.com>
2016-03-03 16:05:56 +0100

    Updated secretary to allow running without encryption

--- demo-webapp/launch.sh
+++ demo-webapp/launch.sh
@@ -2,8 +2,12 @@
 set -e -x
 env
 
-# Decrypt environment variables
-SECRETS=$(secretary decrypt -e --service-key=/service/keys/service-private-key.pem)
+# Decrypt secrets
+if [ "$SERVICE_PUBLIC_KEY" != "" ]; then
+    SECRETS=$(secretary decrypt -e --service-key=/service/keys/service-private-key.pem)
+else
+    SECRETS=$(secretary decrypt -e)
+fi
 
 # Output the secrets to the container log for debug purposes (don't do this in any remotely real situation!)
 echo "Decrypted secrets are:"
